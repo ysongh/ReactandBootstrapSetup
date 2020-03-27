3872b63eb2de85e4b385becb8798895fa01db08b
You Song Hou <yousonghou@yahoo.com>
2020-03-27 02:14:28 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route  } from 'react-router-dom';
 
+import Navbar from './components/layout/Navbar';
 import Landing from './components/Landing';
 import Stories from './components/Stories';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Route exact path="/" component={Landing} />
       <Route exact path="/stories" component={Stories} />
     </Router>
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="/">Voice Over Virus</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/stories">Stories</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
