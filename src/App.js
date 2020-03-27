f6469d9af769b3592c790f13bb9cbaaf97163d69
You Song Hou <yousonghou@yahoo.com>
2020-03-27 01:28:01 -0400

    install react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route  } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <h1>Voice Over Virus</h1>
-    </>
+    </Router>
   );
 }
 
